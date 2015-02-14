0cc0ddaeeb3b2b5207053c44c8c018ed613995d2
tzabrodskaya <tamara.zabrodskaya@gmail.com>
2015-02-14 16:49:54 +0100

    recomendations: extended .gitignore plus updated parameter name

--- addressbook-selenium-tests/src/com/example/tests/GroupData.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupData.java
@@ -11,8 +11,8 @@ public class GroupData {
 	public String header = "";
 	public String footer = "";
 
-	public GroupData(String groupName, String header, String footer) {
-		this.name = groupName;
+	public GroupData(String name, String header, String footer) {
+		this.name = name;
 		this.header = header;
 		this.footer = footer;
 	}
